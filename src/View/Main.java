7bd87343323e15ece4fecd959a1ed3338b4d6528
Henrique Alvaro <henrique.coolcat15@gmail.com>
2023-02-08 22:43:06 -0300

    construindo classe model
    quase todas as classes do pacote model

---
+++ src/View/Main.java
@@ -0,0 +1,17 @@
+package View;
+
+import Model.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "tiago";
+        
+        Servico barba = new Servico(1, "barba", 30);
+        
+        Cliente cliente = new Cliente(1, "Henrique", 'm', "61998825651", "qnp conj f casa 16");
+        
+        Usuario usuario = new Usuario(1, "Alvaro", "jubirildo");
+        System.out.println("Nome: "+usuario.getNome()); 
+        System.out.println("Senha: "+usuario.getSenha());
+    }
+}
